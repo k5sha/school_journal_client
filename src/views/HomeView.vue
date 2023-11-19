6a7f8ec9eeab9d554af73c277e546cb38037ef8e
yurri <gositer2003@gmail.com>
2023-11-19 23:58:12 +0200

    Big update 📅 +Typescript

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,5 @@
 <template>
-    <button
-        class="btn btn-primary"
-        type="button"
-        data-bs-toggle="offcanvas"
-        data-bs-target="#offcanvasScrolling"
-        aria-controls="offcanvasScrolling"
-    >
-        Новини
-    </button>
-
-    <div
-        class="offcanvas offcanvas-start"
-        data-bs-scroll="true"
-        data-bs-backdrop="false"
-        tabindex="-1"
-        id="offcanvasScrolling"
-        aria-labelledby="offcanvasScrollingLabel"
-    >
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Меню</h5>
-            <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="offcanvas"
-                aria-label="Close"
-            ></button>
-        </div>
-        <div class="offcanvas-body">
-            <p>Новини</p>
-        </div>
-    </div>
+    <h1>Пуста сторінка</h1>
 </template>
 
 <script lang="ts">
